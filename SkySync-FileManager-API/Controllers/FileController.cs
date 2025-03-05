8a665c53c222cccc061aea49ed145473d048810f
Guna Sekhar Reddy Peddireddygari <101317857+gunasekhar02@users.noreply.github.com>
2025-03-05 22:45:27 +0530

    SKY-01 added filepath

--- SkySync-FileManager-API/Controllers/FileController.cs
+++ SkySync-FileManager-API/Controllers/FileController.cs
@@ -64,6 +64,7 @@ namespace SkySync_FileManager_API.Controllers
                     f.Id,
                     f.FileName,
                     f.FileType,
+                    f.FilePath,
                     FileSize = GetReadableFileSize(f.FileSize),
                     f.UploadDate
                 })
@@ -134,6 +135,44 @@ namespace SkySync_FileManager_API.Controllers
                 }
             }
 
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteFile([FromQuery] string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return BadRequest("File path is required.");
+            }
+
+            try
+            {
+                var accessToken = _config["Dropbox:AccessToken"];
+                using (var dropboxClient = new DropboxClient(accessToken))
+                {
+                    // Delete the file
+                    await dropboxClient.Files.DeleteV2Async(filePath);
+                    var fileInfo = await _context.FileInfos
+                        .FirstOrDefaultAsync(f => f.FilePath == filePath);
+
+                    if (fileInfo != null)
+                    {
+                        _context.FileInfos.Remove(fileInfo);
+                        await _context.SaveChangesAsync();
+                    }
+
+                    return Ok($"File '{filePath}' deleted successfully from Dropbox and SQL.");
+                }
+
+            }
+            catch (Dropbox.Api.ApiException<DeleteError> ex)
+            {
+                return StatusCode(500, $"Error deleting file: {ex.ErrorResponse}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
         }
 
         private static string GetReadableFileSize(long sizeInBytes)
